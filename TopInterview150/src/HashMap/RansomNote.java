86dec2510685ea324508fe1841e8a1b8f6f11643
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-09 15:02:16 -0400

    Adding files into hashmap

---
+++ TopInterview150/src/HashMap/RansomNote.java
@@ -0,0 +1,35 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        String a = "ba";
+        String b = "baa";
+
+        System.out.println(canConstruct(a, b));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> ransomMap = new HashMap<>();
+        Map<Character, Integer> magazineMap = new HashMap<>();
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            ransomMap.put(ransomNote.charAt(i), ransomMap.getOrDefault(ransomNote.charAt(i), 0) + 1);
+        }
+
+
+        for (int i = 0; i < magazine.length(); i++) {
+            magazineMap.put(magazine.charAt(i), magazineMap.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+
+        for (Character key : ransomMap.keySet()) {
+            if (!magazineMap.containsKey(key) || ransomMap.get(key) > magazineMap.get(key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
