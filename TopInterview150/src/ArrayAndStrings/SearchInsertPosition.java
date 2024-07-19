dda1c80c1b0f0747bc0224db52d8f69f5f80dc24
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-18 21:04:55 -0400

    Update SearchInsertPosition.java

--- TopInterview150/src/ArrayAndStrings/SearchInsertPosition.java
+++ TopInterview150/src/ArrayAndStrings/SearchInsertPosition.java
@@ -3,25 +3,45 @@ package ArrayAndStrings;
 public class SearchInsertPosition {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6};
-        int target = 7;
+        //int target = 7;
         //int target=0;
-        //int target=2;
+        //int target=5;
+        int target=2;
 
-        System.out.println(findPositionBF(nums, target));
+        System.out.println(findPositionBS(nums, target));
 
     }
 
 
-    public static int findPositionBF(int[] nums, int target){
+    public static int findPositionBF(int[] nums, int target) {
         //O(n)
-        for(int i=0;i< nums.length;i++){
-            if(nums[i]>=target){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
                 return i;
             }
         }
         return nums.length;
     }
 
- // ToDO: solve it with Binary Search  O(n logn)
+    // ToDO: solve it with Binary Search  O(n logn)
+
+    public static int findPositionBS(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+           int mid = left + (right - left) / 2;
+            if (target < nums[mid]) {
+                right = mid-1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+
+        return left;
+   }
 }
 
