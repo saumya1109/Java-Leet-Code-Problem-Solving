ce6160694fd0520747591284632b627b742b022c
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-29 13:25:22 -0400

    Adding traversal with Queue

--- TopInterview150/src/BinaryTreeGeneral/MaximumDeptOfBinaryTree.java
+++ TopInterview150/src/BinaryTreeGeneral/MaximumDeptOfBinaryTree.java
@@ -1,5 +1,8 @@
 package BinaryTreeGeneral;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDeptOfBinaryTree {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
@@ -8,7 +11,8 @@ public class MaximumDeptOfBinaryTree {
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
 
-        System.out.println(maxDepth(root));
+        //System.out.println(maxDepth(root));
+        System.out.println(maxDeptWithQueue(root));
 
 
     }
@@ -23,6 +27,41 @@ public class MaximumDeptOfBinaryTree {
         int right = maxDepth(root.right);
         return Math.max(left, right) + 1;
 
+    }
+
+    public static int maxDeptWithQueue(TreeNode root){
+
+        //If there is no elememt in the tree return 0;
+       if(root==null){
+           return 0;
+       }
+       //Add the root node to the Queue
+
+        //Queue<--3
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int dept=0;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            dept++;
+
+            while (size-- > 0) {
+                TreeNode removed = queue.remove();
+
+                if (removed.left != null) {
+                    queue.add(removed.left);
+
+                }
+
+                if (removed.right != null) {
+                    queue.add(removed.right);
+                }
+            }
+        }
+
+        return dept;
 
     }
 }
